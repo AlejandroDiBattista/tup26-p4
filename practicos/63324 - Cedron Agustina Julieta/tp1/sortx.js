a34668f08d37bbbf1f9fc4eb06cad474c2a428bd
Agustina-Cedron <agustinacedron7@gmail.com>
2026-09-04 21:39:37 -0300

    Validar los argumentos de entrada

--- practicos/63324 - Cedron Agustina Julieta/tp1/sortx.js
+++ practicos/63324 - Cedron Agustina Julieta/tp1/sortx.js
@@ -35,4 +35,52 @@ EJEMPLOS:
 `
 
 // Escribir aqui la solución al enunciado.
+//1-parseArgs:
+function parseArgs(argumentos){
+    //aqui voy a guardar todo lo que me mande el usuario
+    const opciones={
+        arcEntrada:null,
+        arcSalida:null,
+        ayuda:false,
+        reglas:[]
+    };
+    
+    const valores=[];
+    //recorro todo lo que escribieron
+    for(let i=0; i<argumentos.length;i++){
+        const arg=argumentos[i];
+        //verifico si pidio ayuda o no
+        if(arg==="-h"||arg==="--help"){
+            opciones.ayuda=true;
+        }
+        // si es para ordenar, uso la regla
+        else if (arg==="-b"||arg==="--by") {
+            //avazo para guardar el dato siguientee
+            i++;
+            const valor=argumentos[i];
+            if (valor) {
+                //desarmo el texto separado por los dos puntos
+                const [nombre,tipo, orden]=valor.split(":");
+                opciones.reglas.push({
+                    nombre:nombre,
+                    tipo:tipo === "num",
+                    orden:orden ==="desc"
+            }); 
+            }
+        //si no tiene guión, es porque es un archivo
+        }else if(!arg.startsWith("-")){
+            valores.push(arg);
+        }
+    }
+    //el primero que pasen es el archivo a leer
+        if(valores.length>0){
+            opciones.arcEntrada=valores[0];
+        }
+        //si pasan otro, es el de salida
+        if(valores.length>1){
+            opciones.arcSalida=valores[1];
+        }
+        return opciones;
+    
+}
 console.log(HELP)
