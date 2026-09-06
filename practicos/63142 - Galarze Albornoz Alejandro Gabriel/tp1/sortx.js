99e7dfc4d0c215ca684077f89e3d12b312719f7b
AleGalarze <alegalarzeoficial@gmail.com>
2026-09-06 17:13:57 -0300

    terminando parseArgs y comenzando readInput

--- practicos/63142 - Galarze Albornoz Alejandro Gabriel/tp1/sortx.js
+++ practicos/63142 - Galarze Albornoz Alejandro Gabriel/tp1/sortx.js
@@ -35,7 +35,8 @@ EJEMPLOS:
 `
 
 // Escribir aqui la solución al enunciado.
-// console.log(HELP) 
+// console.log(HELP);
+import fs from "fs";
 
 function parseArgs(args){
     if (args.includes('-h') || args.includes('--help')) {
@@ -45,29 +46,68 @@ function parseArgs(args){
 
     const positional = [];
     const sortFieldsRaw = [];
-    
+    let delimiter = ',';
+    let noHeader = false;
+
     for (let i = 0; i < args.length; i++) {
         const arg = args[i];
 
         if (arg === '-b' || arg === '--by') {
             const valor = args [++i];
+
+            if(!valor || valor.startsWith('-')) {
+                throw new Error('Falta el valor de la opción -b.');
+            }
             
             const partes = valor.split(':');
             
             const campo = partes[0];
-            const tipo = partes[1];
-            const orden = partes [2];
+            const tipo = partes[1] || "alpha";
+            const orden = partes [2] || "asc";
+
+            if (!campo) {
+                throw new Error('Falta el nombre del campo en -b')
+            }
+            if (tipo !== 'alpha' && tipo !== 'num') {
+                throw new Error ('el tipo debe ser num o alpha')
+            }
+            if (orden !== 'asc' && orden !== 'desc') {
+                throw new Error ('orden deberia ser desc o asc')
+            }
 
             sortFieldsRaw.push({ campo, tipo, orden });
-        } else if (!arg.startsWith('-')) {
+        }   else if (arg ==='-d' || arg === '--delimiter') {
+                const val = args[++i];
+
+                if (!val) {
+                    throw new Error('Falta el delimitador para -d');
+                }
+                delimiter = val;
+
+        }   else if (arg === '-nh' || arg === '--no-header') {
+            noHeader = true;
+
+        }   else if (!arg.startsWith('-')) {
             positional.push(arg);
         }
+    }
+
+    const sortFields = [];
+    for (let i = 0; i <sortFieldsRaw.length; i++) {
+        const item = sortFieldsRaw[i];
+        sortFields.push({
+        name: item.campo,
+        numeric: item.tipo === 'num',
+        descending: item.orden === 'desc'
+        });
     }
 
     return {
         inputFile: positional [0],
         outputFile: positional [1],
-        sortFieldsRaw: sortFieldsRaw
-    };
+        delimiter: delimiter,
+        sortFields: sortFields,
+        noHeader: noHeader
+    }; 
 
 }
