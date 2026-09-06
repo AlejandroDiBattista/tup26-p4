f6a5bcb6df7b42d3799eaf5ac1004d72679a6a6d
MajoAndrada18 <majoandrada18@gmail.com>
2026-09-06 14:45:55 -0300

    Se prueba y valida opcion help

--- practicos/61543 - Andrada Maria Jose/tp1/sortx.js
+++ practicos/61543 - Andrada Maria Jose/tp1/sortx.js
@@ -40,14 +40,20 @@ function parseArgs() {
      const args = process.argv.slice(2);
      const inputFile = args[0];
      const outputFile = args[1];
+     const sortFields = [];
+     let noHeader = false;
+     if (args.includes("-h") || args.includes("--help")) {
+        console.log(HELP);
+        process.exit(0);
+     }
+
      if(inputFile === undefined) {
         throw new Error("Falta indicar el archivo de origen");
      }
      if (outputFile === undefined) {
         throw new Error("Falta indicar el archivo de destino");
      }
-     const sortFields = [];
-     let noHeader = false;
+
      for (let i = 2; i < args.length; i++) {
           if (args[i] === "-b" || args[i] === "--by") {
             if (args[i + 1] === undefined) {
