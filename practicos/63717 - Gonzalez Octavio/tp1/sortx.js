3687a5f25554d5d8ddbc3c21742bc3ea15da09e9
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-09-06 11:21:23 -0300

    2 - Funcion read input

--- practicos/63717 - Gonzalez Octavio/tp1/sortx.js
+++ practicos/63717 - Gonzalez Octavio/tp1/sortx.js
@@ -36,24 +36,25 @@ EJEMPLOS:
     sortx empleados.csv resultado.csv -b departamento -b salario:num:desc
     sortx datos.csv resultado.csv -nh -b 2:num:desc
     sortx datos.tsv salida.tsv -d "\t" -b nombre
-`
-const argumentos = process.argv.slice(2); // array c/argumentos desde el indice indicado 
-
-// Llamada a funciones ————————————————————————————————————————————————————————————————
-
-parseArgs(argumentos);
-
+    `
+    const argumentos = process.argv.slice(2); // array c/argumentos desde el indice indicado 
+    
+    // Llamada a funciones ————————————————————————————————————————————————————————————————
+    
+    const configuracion = parseArgs(argumentos);
+    const input         = readInput(configuracion.inputFile);
+    const filas         = parseDelimited(input, configuracion);
 // Funciones ————————————————————————————————————————————————————————————————————————
 
 function parseArgs(args) {
     const [entrada, destino, ...opciones] = args;
-
+    
     if (entrada === "h" || entrada === "help" || entrada === "-h" || entrada === "--help") {
         console.clear();
         console.log(HELP);
         process.exit(0);
     }
-
+    
     if (entrada === undefined && destino === undefined) {
         console.error("Debe especificar un archivo de origen y de entrada, sino help para obtener ayuda.");
         process.exit(-1);
@@ -66,84 +67,85 @@ function parseArgs(args) {
         console.error(`
             La ruta debe escribirse como 'ruta', no "ruta" 
             `);
-        process.exit(-1);
-    }
-    if (destino.startsWith('"')) {
-        console.error(`
-            La ruta debe escribirse como 'ruta', no "ruta"
-            `);
-        process.exit(-1);
-    }
-
-    let config = {
-        inputFile: entrada,
-        outputFile: destino,
-        sortfields: [],
-        delimiter: ",",
-        noHeader: false,
-    }
-
-    for (let i = 0; i < opciones.length; i++) {
-
-        if (opciones[i] === "-b" || opciones[i] === "--by") {
-
-            if (opciones[i + 1] === undefined || opciones[i + 1].startsWith("-")) {
-                console.error("La opcion -b debe ir seguida de un criterio de ordenamiento. Consultar help.");
-                process.exit(-1);
-            }
-
-            let [campo, ...modificador] = opciones[i + 1].split(":");
-            const nombre = campo === "" ? undefined : campo;
-
-            if (nombre != undefined) {
-                config.sortfields.push({
-                    name: nombre,
-                    numeric: modificador.includes("num"),
-                    descending: modificador.includes("desc")
-                });
-            }
-            i++;
-            continue;
-        }
-        if (opciones[i] === "-d" || opciones[i] === "--delimiter") {
-            const valor = opciones[i + 1];
-            if (valor === '\\t') config.delimiter = "\t";
-
-            else if (valor === undefined || valor.startsWith("-") || valor.length != 1) {
-                console.error("Debe especificar un delimitador. Consultar help."
-                );
-                process.exit(-1);
-            }
-            else {config.delimiter = valor;}
-            i++;
-            continue;
-        }
-        if (opciones[i] === "-nh" || opciones[i] === "--no-header") {
-            config.noHeader = true;
-            continue;
-        }
-
-        if ((opciones[i] !== "-b" && opciones[i] !== "--by" &&
-            opciones[i] !== "-d" && opciones[i] !== "--delimiter" &&
-            opciones[i] !== "-nh" && opciones[i] !== "--no-header")) {
-            console.log(`
-            Opcion invalida: ${opciones[i]}. Consultar help.`);
             process.exit(-1);
         }
+        if (destino.startsWith('"')) {
+            console.error(`
+                La ruta debe escribirse como 'ruta', no "ruta"
+                `);
+                process.exit(-1);
+            }
+            
+            let config = { inputFile: entrada, outputFile: destino, sortfields: [], delimiter: ",", noHeader: false }
+            
+            for (let i = 0; i < opciones.length; i++) {
+                if (opciones[i] === "-b" || opciones[i] === "--by") {
+                    
+                    if (opciones[i + 1] === undefined || opciones[i + 1].startsWith("-")) {
+                        console.error("La opcion -b debe ir seguida de un criterio de ordenamiento. Consultar help.");
+                        process.exit(-1);
+                    }
+                    
+                    let [campo, ...modificador] = opciones[i + 1].split(":");
+                    const nombre = campo === "" ? undefined : campo;
+                    
+                    if (nombre != undefined) {
+                        config.sortfields.push({
+                            name: nombre,
+                            numeric: modificador.includes("num"),
+                            descending: modificador.includes("desc")
+                        });
+                    }
+                    i++;
+                    continue;
+                }
+                if (opciones[i] === "-d" || opciones[i] === "--delimiter") {
+                    const valor = opciones[i + 1];
+                    if (valor === '\\t') config.delimiter = "\t";
+                    
+                    else if (valor === undefined || valor.startsWith("-") || valor.length != 1) {
+                        console.error("Debe especificar un delimitador. Consultar help."
+                        );
+                        process.exit(-1);
+                    }
+                    else { config.delimiter = valor; }
+                    i++;
+                    continue;
+                }
+                if (opciones[i] === "-nh" || opciones[i] === "--no-header") {
+                    config.noHeader = true;
+                    continue;
+                }
+                
+                if ((opciones[i] !== "-b" && opciones[i] !== "--by" &&
+                    opciones[i] !== "-d" && opciones[i] !== "--delimiter" &&
+                    opciones[i] !== "-nh" && opciones[i] !== "--no-header")) {
+                        console.log(`
+                            Opcion invalida: ${opciones[i]}. Consultar help.`);
+                            process.exit(-1);
+                        }
+                        
+                        
+                    }
+                    console.log(config);
+                    return config;
+                }
+           
+async function readInput(entrada) {
 
-
-    }
-    console.log(config);
-    return config;
+    let input = await readFile(entrada, 'utf-8');
+    console.log(input);
+    return input;
 }
-// function readInput(entrada) {
 
 
-// }
-// function parseDelimited (input, config)
-// {
+function parseDelimited(input, config)
+{
+    let filas = input.split('\n');
+    let columnas = filas[0].split(config.delimiter);
+    
 
-// }
+}
 // function sortRows (rows, config)
 // {
 
