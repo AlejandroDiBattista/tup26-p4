2b3a88b56d433b4b79b266f0131588f4300b5b40
Hernan Gonzalez <hernangoonzalez@gmail.com>
2026-09-13 19:46:35 -0300

    Cargar y parsear CSV al iniciar, y mostrar barra superior

--- practicos/63630 - Gonzalez Pablo Hernan/tp2/edit.jsx
+++ practicos/63630 - Gonzalez Pablo Hernan/tp2/edit.jsx
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S node --import tsx
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {render, Box, Text, useInput, useApp} from 'ink';
 import {readFile, writeFile} from 'node:fs/promises';
 import {TextInput} from '@inkjs/ui';
@@ -19,6 +19,51 @@ const COLORES = {
 
 function App() {
     const {exit} = useApp();
+
+
+    const [modo, setModo] = useState('navegando');
+    const [encabezado, setEncabezado] = useState([]);
+    const [filas, setFilas] = useState([]);
+    const [filaSeleccionada, setFilaSeleccionada] = useState(0);
+    const [columnaSeleccionada, setColumnaSeleccionada] = useState(0);
+    const [nombreArchivo, setNombreArchivo] = useState(null);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+    const archivo = process.argv[2];
+    setNombreArchivo(archivo);
+
+    async function cargar() {
+
+        if(archivo != undefined){
+
+        const contenido = await readFile(archivo, 'utf8')
+        
+        const lineas = contenido.split(/\r?\n/);
+        const primeraLinea = lineas[0].split(',');
+        const restoDeLineas = lineas.slice(1)
+        .filter((linea) => linea !== '')
+        .map((linea) => linea.split(','));
+
+
+        setEncabezado(primeraLinea);
+        setFilas(restoDeLineas);
+
+
+        }
+
+    }
+    cargar();
+
+    }, []);
+
+
+
+
+
+
+
     
     useInput((tecla, key) => {
         if (key.escape) {
@@ -27,14 +72,21 @@ function App() {
     })
 
     return (
-        <Box width={COLUMNAS} height={FILAS} justifyContent="center" alignItems="center">
-            <Box width={40} height={10} flexDirection="column" borderStyle="round" borderColor={COLORES.borde} backgroundColor={COLORES.fondo}>
+    <Box flexDirection="column" width={COLUMNAS} borderStyle="round" borderColor={COLORES.borde} backgroundColor={COLORES.fondo}>
+        <Box flexDirection="row" justifyContent="space-between">
+        <Text>{nombreArchivo}</Text>
+        <Text>{filas.length} filas · {encabezado.length} columnas</Text>
+        </Box>
+        
+        <Box width={COLUMNAS} justifyContent="center" alignItems="center">
+            <Box>
                 <Box flexGrow={1} justifyContent="center" alignItems="center">
                     <Text bold color={COLORES.titulo}>Editor CSV</Text>
                 </Box>
                 <Text color={COLORES.secundario}><Text bold color={COLORES.acento}> Esc</Text> salir</Text>
             </Box>
         </Box>
+    </Box>
     );
 }
 
