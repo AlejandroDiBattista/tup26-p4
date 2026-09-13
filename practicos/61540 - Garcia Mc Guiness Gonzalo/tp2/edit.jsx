74c8398cbfbc95fac07e0cfbc1589a0556e7ba15
Gonz4McGuiness <garciamcguiness13@gmail.com>
2026-09-13 12:59:49 -0300

    Commit 3
    - Correccion para adaptar la tabla  en la terminal.

--- practicos/61540 - Garcia Mc Guiness Gonzalo/tp2/edit.jsx
+++ practicos/61540 - Garcia Mc Guiness Gonzalo/tp2/edit.jsx
@@ -6,8 +6,6 @@ import {readFile, writeFile} from 'node:fs/promises';
 import {TextInput} from '@inkjs/ui';
 import {basename} from 'node:path';
 
-const COLUMNAS = process.stdout.columns || 80;
-const FILAS    = process.stdout.rows || 24;
 
 const COLORES = {
     fondo:     '#161310',
@@ -25,11 +23,12 @@ function App() {
     const [contenido, setContenido] = useState('')
     const [cabecera, setCabecera] = useState([])
     const [filas, setFilas] = useState([])
+    const [columnas, setColumnas] = useState(process.stdout.columns || 80)
 
     const ancho = 4
     const separacion = 2
     const anchoColumna = Math.floor(
-        (COLUMNAS - ancho - separacion * cabecera.length) / cabecera.length
+        (columnas - ancho - separacion * cabecera.length) / cabecera.length
     )
 
     async function abrirArchivo(nombre) {
@@ -53,6 +52,17 @@ function App() {
         }
     }, [])
 
+    useEffect(() => {
+        const actualizarTamaño = () => {
+            setColumnas(process.stdout.columns || 80)
+        }
+        
+        process.stdout.on('resize', actualizarTamaño)
+        return () => {
+            process.stdout.off('resize', actualizarTamaño)
+        }
+    }, [])
+
     useInput((tecla, key) => {
         if (key.escape) {
             exit();
@@ -73,8 +83,8 @@ function App() {
     }
 
     return (
-        <Box flexDirection="column" width={COLUMNAS} borderStyle="round" borderColor={COLORES.borde}>
-            <Box flexDirection="row" gap={70} paddingBottom={2}>
+        <Box flexDirection="column" width={columnas} borderStyle="round" borderColor={COLORES.borde}>
+            <Box flexDirection="row" justifyContent="space-between" paddingBottom={2}>
                 <Text color={COLORES.titulo}>Archivo: {nombreArchivo}</Text>  
                 <Text color={COLORES.secundario}>
                     Columnas: {cabecera.length} | Filas: {filas.length}
