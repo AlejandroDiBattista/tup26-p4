2f9625baa0aeb76db60deec6222efdd5ba9b3452
Pablo Collante <pablocollante.dev@gmail.com>
2026-09-13 12:42:59 -0300

    estado y lectura inicial

--- practicos/63143 - Collante Pablo Augusto/tp2/edit.jsx
+++ practicos/63143 - Collante Pablo Augusto/tp2/edit.jsx
@@ -1,25 +1,44 @@
 #!/usr/bin/env -S node --import tsx
 
-import React from 'react';
-import {render, Box, Text, useInput, useApp} from 'ink';
-import {readFile, writeFile} from 'node:fs/promises';
-import {TextInput} from '@inkjs/ui';
-import {basename} from 'node:path';
+import React, { useState, useEffect } from 'react';
+import { render, Box, Text, useInput, useApp } from 'ink';
+import { readFile, writeFile } from 'node:fs/promises';
+import { TextInput } from '@inkjs/ui';
+import { basename } from 'node:path';
 
 const COLUMNAS = process.stdout.columns || 80;
-const FILAS    = process.stdout.rows || 24;
+const FILAS = process.stdout.rows || 24;
 
 const COLORES = {
-    fondo:     '#161310',
-    borde:     '#726b61',
-    titulo:    '#ede7db',
-    secundario:'#ada79e',
-    acento:    '#edbb64',
+    fondo: '#161310',
+    borde: '#726b61',
+    titulo: '#ede7db',
+    secundario: '#ada79e',
+    acento: '#edbb64',
 };
 
 function App() {
-    const {exit} = useApp();
-    
+    const { exit } = useApp();
+
+    const [archivo, setArchivo] = useState(process.argv[2] || '');
+    const [datosCsv, setDatosCsv] = useState([["Columna 1", "Columna 2"], ["Fila 1", "Fila 2"]]);
+    const [seleccion, setSeleccion] = useState({ fila: 0, columna: 0 });
+    useEffect(() => {
+        async function cargarArchivo() {
+            if (!archivo) return;
+
+            try {
+                const contenido = await readFile(archivo, 'utf-8');
+                const lineas = contenido.trim().split('\n');
+                const matriz = lineas.map(linea => linea.split(','));
+                setDatosCsv(matriz);
+            } catch (error) { }
+        }
+        cargarArchivo();
+    }, [archivo]);
+
+
+
     useInput((tecla, key) => {
         if (key.escape) {
             exit();
