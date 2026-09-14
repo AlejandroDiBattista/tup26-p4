50c3cf6f96921319084a58ae0cf226d9357187fe
Pablo Collante <pablocollante.dev@gmail.com>
2026-09-14 20:21:27 -0300

    retorno de carro (\r) al parsear el CSV para arreglar celdas rotas

--- practicos/63143 - Collante Pablo Augusto/tp2/edit.jsx
+++ practicos/63143 - Collante Pablo Augusto/tp2/edit.jsx
@@ -15,7 +15,11 @@ const COLORES = {
     titulo: '#ede7db',
     secundario: '#ada79e',
     acento: '#edbb64',
+    celdaSeleccionada: '#ffffff',
+    textoOscuro: '#000000'
 };
+
+const WINDOW_SIZE = 13;
 
 function App() {
     const { exit } = useApp();
@@ -23,12 +27,14 @@ function App() {
     const [archivo, setArchivo] = useState(process.argv[2] || '');
     const [datosCsv, setDatosCsv] = useState([["Columna 1", "Columna 2"], ["Fila 1", "Fila 2"]]);
     const [seleccion, setSeleccion] = useState({ fila: 0, columna: 0 });
+    const [modo, setModo] = useState("vista");
+
     useEffect(() => {
         async function cargarArchivo() {
             if (!archivo) return;
             try {
                 const contenido = await readFile(archivo, 'utf-8');
-                const lineas = contenido.trim().split('\n');
+                const lineas = contenido.trim().replace(/\r/g, '').split('\n');
                 const matriz = lineas.map(linea => linea.split(','));
                 setDatosCsv(matriz);
             } catch (error) { }
@@ -40,42 +46,97 @@ function App() {
 
     useInput((tecla, key) => {
         if (key.escape) {
-            exit();
+            if (modo === "vista") {
+                exit();
+            } else {
+                setModo("vista");
+            }
         }
 
-        if (key.upArrow) {
-            setSeleccion(prev => ({ ...prev, fila: Math.max(prev.fila - 1, 0) }));
+        if (modo === "vista") {
+            if (key.upArrow) {
+                setSeleccion(prev => ({ ...prev, fila: Math.max(prev.fila - 1, 0) }));
+            }
+            if (key.downArrow) {
+                setSeleccion(prev => ({ ...prev, fila: Math.min(prev.fila + 1, datosCsv.length - 1) }));
+            }
+            if (key.leftArrow) {
+                setSeleccion(prev => ({ ...prev, columna: Math.max(prev.columna - 1, 0) }));
+            }
+            if (key.rightArrow) {
+                setSeleccion(prev => ({ ...prev, columna: Math.min(prev.columna + 1, datosCsv[0].length - 1) }));
+            }
+
+            if (tecla && tecla.toLocaleLowerCase() === 'a' && modo === "vista") {
+                setModo("abrir");
+            }
+            if (tecla && tecla.toLocaleLowerCase() === 'g' && modo === "vista") {
+                setModo("guardar");
+            }
         }
-        if (key.downArrow) {
-            setSeleccion(prev => ({ ...prev, fila: Math.min(prev.fila + 1, datosCsv.length - 1) }));
-        }
-        if (key.leftArrow) {
-            setSeleccion(prev => ({ ...prev, columna: Math.max(prev.columna - 1, 0) }));
-        }
-        if (key.rightArrow) {
-            setSeleccion(prev => ({ ...prev, columna: Math.min(prev.columna + 1, datosCsv[0].length - 1) }));
-        }
-    })
+    });
 
     return (
-        <Box width={COLUMNAS} height={FILAS} flexDirection="column" backgroundColor={COLORES.fondo}>
-            <Box marginBottom={1}><Text color={COLORES.titulo}>Archivo: {archivo || 'Ninguno'}</Text></Box>
-            {datosCsv.map((fila, i) => (
-                <Box key={i} flexDirection="row">
-                    {fila.map((celda, j) => (
-                        <Box key={j} width={20} height={3} borderStyle="round" borderColor={COLORES.borde} backgroundColor={seleccion.fila === i && seleccion.columna === j ? COLORES.acento : COLORES.fondo}>
-                            <Text>{celda}</Text>
-                        </Box>
-                    ))}
+        <Box width={COLUMNAS} flexDirection="column" backgroundColor={COLORES.fondo}>
+            {modo === 'vista' ? (
+                <Box flexDirection="column">
+                    <Box marginBottom={1}><Text color={COLORES.titulo}>Archivo: {archivo || 'Ninguno'}</Text></Box>
+                    {datosCsv.slice(seleccion.fila, seleccion.fila + 7).map((fila, i) => {
+                        const indiceReal = seleccion.fila + i;
+                        return (
+                            <Box key={indiceReal} flexDirection="row">
+                                {fila.map((celda, j) => (
+                                    <Box key={j} width={14} borderStyle="single" borderColor={COLORES.borde} backgroundColor={seleccion.fila === indiceReal && seleccion.columna === j ? COLORES.acento : undefined}>
+                                        <Text color={seleccion.fila === indiceReal && seleccion.columna === j ? COLORES.fondo : COLORES.titulo}>
+                                            {celda}
+                                        </Text>
+                                    </Box>
+                                ))}
+                            </Box>
+                        );
+                    })
+                    }
+
+                    <Box marginTop={1} paddingX={1} borderStyle="single" borderColor={COLORES.secundario}>
+                        <Text color={COLORES.acento}> Posicion: [{seleccion.fila}, {seleccion.columna}] | Valor: {datosCsv[seleccion.fila]?.[seleccion.columna]}</Text>
+                    </Box>
                 </Box>
-            ))}
-            <Box marginTop={1} paddingX={1} borderStyle="single" borderColor={COLORES.secundario}>
-                <Text color={COLORES.acento}> Posicion: [{seleccion.fila}, {seleccion.columna} | Valor: {datosCsv[seleccion.fila]?.[seleccion.columna]}]</Text>
-            </Box>
+            ) : (
+                <Box flexGrow={1} justifyContent="center" alignItems="center">
+                    <Box borderStyle="round" borderColor={COLORES.borde} padding={1}>
+                        <Text>{modo === 'abrir' ? 'Abrir archivo: ' : 'Guardar archivo: '}</Text>
+                        <TextInput
+                            placeholder="empleados.csv"
+                            onSubmit={async (valor) => {
+                                if (modo === "abrir") {
+                                    try {
+                                        const contenido = await readFile(valor, 'utf-8');
+                                        const matriz = contenido.trim().replace(/\r/g, '').split('\n').map(linea => linea.split(','));
+                                        setDatosCsv(matriz);
+                                        setArchivo(valor);
+                                        setSeleccion({ fila: 0, columna: 0 });
+                                        setModo("vista");
+                                    } catch (error) {
+                                        setModo("vista");
+                                    }
+                                } else if (modo === "guardar") {
+                                    try {
+                                        const texto = datosCsv.map(f => f.join(',')).join('\n');
+                                        await writeFile(valor, texto, 'utf-8');
+                                        setArchivo(valor);
+                                        setModo("vista");
+                                    } catch (error) {
+                                        setModo("vista");
+                                    }
+                                }
+                            }}
+                        />
+                    </Box>
+                </Box>
+            )}
         </Box>
     );
 }
-
 
 
 const app = render(<App />);
