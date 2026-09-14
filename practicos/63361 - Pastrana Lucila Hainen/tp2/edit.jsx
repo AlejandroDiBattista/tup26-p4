b767efc41e6baba8488490874909337877ccfbca
lucilapastranaok-hue <lucilapastrana.ok@gmail.com>
2026-09-14 13:15:38 -0300

    agrego la lectura del archivo CSV

--- practicos/63361 - Pastrana Lucila Hainen/tp2/edit.jsx
+++ practicos/63361 - Pastrana Lucila Hainen/tp2/edit.jsx
@@ -5,9 +5,14 @@ import {render, Box, Text, useInput, useApp} from 'ink';
 import {readFile, writeFile} from 'node:fs/promises';
 import {TextInput} from '@inkjs/ui';
 import {basename} from 'node:path';
+import { fail } from 'node:assert';
 
 const COLUMNAS = process.stdout.columns || 80;
 const FILAS    = process.stdout.rows || 24;
+const archivo = process.argv[2]; 
+const datos = await readFile(archivo, 'utf8');
+const lineas = datos.trim().split('\n');
+const filas = lineas.map(linea => linea.split(',')); 
 
 const COLORES = {
     fondo:     '#161310',
@@ -15,6 +20,8 @@ const COLORES = {
     titulo:    '#ede7db',
     secundario:'#ada79e',
     acento:    '#edbb64',
+
+ 
 };
 
 function App() {
@@ -29,8 +36,15 @@ function App() {
     return (
         <Box width={COLUMNAS} height={FILAS} justifyContent="center" alignItems="center">
             <Box width={40} height={10} flexDirection="column" borderStyle="round" borderColor={COLORES.borde} backgroundColor={COLORES.fondo}>
-                <Box flexGrow={1} justifyContent="center" alignItems="center">
-                    <Text bold color={COLORES.titulo}>Editor CSV</Text>
+               
+                <Box flexDirection= " colum ">
+                    {filas.map((fila, indice) => (
+                          <Text  key={indice}>
+                            {fila.join(' | ')}
+                    </Text> 
+
+                    ))}
+                  
                 </Box>
                 <Text color={COLORES.secundario}><Text bold color={COLORES.acento}> Esc</Text> salir</Text>
             </Box>
